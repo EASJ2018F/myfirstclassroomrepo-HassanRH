bbfa35676af158839909421b9cff773e5d66f786
HassanRH <hassan---06@hotmail.com>
2018-01-29 10:39:38 +0100

    hello world

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -13,6 +13,7 @@ namespace MyFirstGitHubClassRoomConsoleApp
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
             //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
             Console.WriteLine("Hello world");
+            Console.ReadKey();
         }
     }
 }
